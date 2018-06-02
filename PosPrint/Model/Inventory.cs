ddce421432622db171de87973a7bc9b7a93a0b93
Daniel Adoboah-Seesi <adoboahseesi@hotmail.com>
2018-06-02 11:17:37 +0000

    Added Inventory Model and PurchaseViewModel

---
+++ PosPrint/Model/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosPrint.Model
+{
+    class Inventory
+    {
+
+        public Guid BranchID { get; set; }
+        public Guid InventoryID { get; set; }
+        public DateTime  Date{ get; set; }
+        public Guid ProductID { get; set; }
+        public Guid UserID { get; set; }
+        
+        public string TransactionType { get; set; }
+        public int Quantity { get; set; }
+        public int Balance { get; set; }
+
+       
+        
+    }
+}
